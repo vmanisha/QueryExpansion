9d6a0eeffb2e276e37ed65a8ecbfca0961f00083
vmanisha <manisha.verma@research.iiit.ac.in>
2015-03-01 18:17:02 +0000

    Fixed bugs

--- entity/expand/catClusExpansion.py
+++ entity/expand/catClusExpansion.py
@@ -5,23 +5,37 @@ class ScoreClusterTerms:
 	def __init__(self):
 		print 'Initializing cluster expansion'
 	
+	def getTerms(self, clust):
+		terms = {}
+		for entry in clust:
+			split = entry.split()
+			for st in split:
+				if len(st) > 2:
+					if st not in terms:
+						terms[st]= 0.0
+					terms[st]+=1.0
+		return terms;
+
 	def score(self,query, clustList, scorer, limit):
 		i = 0
 		scores = {} #contains score of clusters
 		order = {}  #contains the order of terms
+		qSet = query.split()
+
 		for clust in clustList:
 			clusScore = 0.0
 			tDict = {}
-			for entry in clust:
-				score, terms = scorer.score(query,entry)
-				#if score > 0:
-				#	print entry, score, terms
-				clusScore+= score
-				
-				for t in terms.keys():
-					if t not in tDict:
-						tDict[t] = 0.0
-					tDict[t]+=terms[t]
+			cTerms = self.getTerms(clust)
+			if i % 5000 == 0:
+				print i,len(clust), len(cTerms)
+			score, terms = scorer.score(qSet,cTerms)
+			#if score > 0:
+			#	print entry, score, terms
+			clusScore+= score
+			for t in terms.keys():
+				if t not in tDict:
+					tDict[t] = 0.0
+				tDict[t]+=terms[t]
 			
 			rTerms = sorted(tDict.items(),reverse = True, key = lambda x : x[1])	
 			order[i] = []
@@ -43,6 +57,6 @@ class ScoreClusterTerms:
 				
 			if len(topTerms) > limit:
 				break
-		print query, topTerms	
+		#print query, topTerms	
 		return topTerms
 	
--- entity/expand/scorer.py
+++ entity/expand/scorer.py
@@ -13,25 +13,24 @@ class CoOccurSimScore:
 	def __init__(self, coOcMan):
 		self.coMan = coOcMan
 	
-	def score(self, text1, text2):
-		terms1 = set(text1.split())
-		terms2 = set(text2.split())
+	def score(self, terms1, terms2):
 		score = 0.0
 		termScore = {}
+		total = sum(terms2.values())
 		#print terms1, len(terms2)
 		for t1 in terms1:
 			if len(t1) > 2:
 				for t2 in terms2:
-					if len(t2) > 2:
-						sc = self.coMan.getCoOcCount(t1,t2)[0]
-						#if sc > 0:
-						#	print t1, t2, sc 
-						score+= sc 
-						if t2 not in termScore:
-							termScore[t2] = 0.0
-						termScore[t2] += score
+					sc = self.coMan.getCoOcCount(t1,t2)[0]
+					#if sc > 0:
+					#	print t1, t2, sc 
+					score+= (sc*(terms2[t2]/total)) 
+					if t2 not in termScore:
+						termScore[t2] = 0.0
+					termScore[t2] += score
 				
 		for entry in termScore.keys():
 			termScore[entry]/= len(terms1)
-	
-		return score/(1.0* len(terms1) * len(terms2)), termScore
+		if len(terms2) > 0 and len(terms1) > 0:
+			return score/(1.0* len(terms1) * len(terms2)), termScore
+		return 0.0, termScore
